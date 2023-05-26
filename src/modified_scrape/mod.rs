a32459463eda57fda6b65676f9f5479cbb539e58
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-05-26 11:30:04 +0200

    Create mod.rs

---
+++ src/modified_scrape/mod.rs
@@ -0,0 +1,14 @@
+pub mod errors;
+pub mod srs;
+pub mod poly;
+pub mod config;
+
+pub mod participant;
+pub mod dealer;
+//pub mod aggregator;
+
+//pub mod pvss;
+pub mod decomp;
+//pub mod share;
+
+//pub mod node;
