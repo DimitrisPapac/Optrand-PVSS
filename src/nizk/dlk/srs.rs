7bc0409567617773b38a81eccc31373b1311e70d
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-06-05 23:15:29 +0200

    Create srs.rs

---
+++ src/nizk/dlk/srs.rs
@@ -0,0 +1,20 @@
+use crate::nizk::utils::errors::NIZKError;
+use ark_ec::AffineCurve;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Read, SerializationError, Write};
+use rand::Rng;
+
+#[derive(Debug, CanonicalSerialize, CanonicalDeserialize, Clone, PartialEq)]
+pub struct SRS<C: AffineCurve> {
+    pub g_public_key: C,   // group generator
+}
+
+impl<C: AffineCurve> SRS<C> {
+
+    // Function setup samples the SRS generator
+    pub fn setup<R: Rng>(_: &mut R) -> Result<Self, NIZKError> {
+        let srs = Self {
+            g_public_key: C::prime_subgroup_generator(),
+        };
+        Ok(srs)
+    }
+}
