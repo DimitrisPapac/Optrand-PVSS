c8430bf7f8bc5959c73edfefe7eb5ac4f865500a
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-06-20 13:30:15 +0200

    Update mod.rs

--- src/modified_scrape/mod.rs
+++ src/modified_scrape/mod.rs
@@ -2,13 +2,12 @@ pub mod errors;
 pub mod srs;
 pub mod poly;
 pub mod config;
+pub mod decomp;
+
+pub mod pvss;
+pub mod share;
 
 pub mod participant;
 pub mod dealer;
 //pub mod aggregator;
-
-pub mod pvss;
-pub mod decomp;
-//pub mod share;
-
 //pub mod node;
