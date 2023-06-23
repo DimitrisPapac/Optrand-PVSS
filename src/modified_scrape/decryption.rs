2e0936f4e4722fb8406b0855913909d73aae9ca7
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-06-23 08:47:08 +0200

    Add files via upload

---
+++ src/modified_scrape/decryption.rs
@@ -0,0 +1,24 @@
+use crate::Scalar;
+use ark_ff::{Field, PrimeField};
+use ark_ec::{PairingEngine, AffineCurve, ProjectiveCurve};
+
+// Struct DecryptedShare represents a decrypted share obtained when a node cancels out its secret
+// key from some given encrypted share.
+// NOTE: It should be noted that without the use of DLEQs, it is not possible to define verification
+// of decryptions.
+#[derive(Clone)]
+pub struct DecryptedShare<E: PairingEngine> {
+    dec: E::G1Affine,   // the decrypted share
+    origin: usize,      // index in the pk_map
+}
+
+impl<E: PairingEngine> DecryptedShare<E> {
+
+    // Associated function for generating a decrypted share from a given encrypted share.
+    fn generate(enc: &E::G1Affine, sk: &Scalar<E>, my_id: usize) -> DecryptedShare<E> {
+	// dec := enc * sk^{-1}
+	let dec = enc.mul(sk.inverse().unwrap().into_repr()).into_affine();
+
+    	DecryptedShare {dec, origin: my_id}
+    }
+}
