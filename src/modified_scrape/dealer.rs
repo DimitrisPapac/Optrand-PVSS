c50bd04f1051f2903a456ca9b2d12982cec796af
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-09-21 08:46:57 +0200

    Update dealer.rs

--- src/modified_scrape/dealer.rs
+++ src/modified_scrape/dealer.rs
@@ -1,4 +1,4 @@
-use crate::{modified_scrape::participant::Participant, signature::scheme::BatchVerifiableSignatureScheme};
+use crate::{modified_scrape::participant::Participant, SecretKey};
 use ark_ec::PairingEngine;
 
 // Struct Dealer models the aspects of each party in the network, when acting as a dealer
@@ -6,15 +6,12 @@ use ark_ec::PairingEngine;
 #[derive(Clone)]
 pub struct Dealer<
     E: PairingEngine,
-    SSIG: BatchVerifiableSignatureScheme<PublicKey = E::G2Affine, Secret = E::Fr>,
 > {
-    pub private_key_sig: SSIG::Secret,            // Dealer's secret (signing) key
+    pub private_key_sig: SecretKey,    // Dealer's secret (signing) key
+    pub participant: Participant<E>,   // Dealers have participant characteristics (structural composition)
 
     // MAY BE REDUNDANT
     pub accumulated_secret: E::G2Affine,     // Dealer's accumulated secret (in G_2)
-
     // MAY BE REDUNDANT
     pub decryptions: Vec<(usize, E::G1Affine)>,   // Dealer's list of accumulated decryptions, along with the respective participant ids
-
-    pub participant: Participant<E, SSIG>,        // Dealers have participant characteristics (structural composition)
 }
