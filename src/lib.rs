3becf89ef44d495267e039899d53e53d3cce4ecf
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-09-21 08:43:51 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,33 @@ pub mod modified_scrape;
 pub mod signature;
 pub mod nizk;
 
-
-
 use ark_poly::univariate::DensePolynomial;
 use ark_ec::PairingEngine;
 
+// EdDSA imports
+
+use ed25519_dalek as dalek;
+use ed25519_dalek::ed25519;
+use ed25519_dalek::Signer as _;
+use rand::rngs::OsRng;
+use rand::{CryptoRng, RngCore};
+use serde::{de, ser, Deserialize, Serialize};
+use std::array::TryFromSliceError;
+use std::convert::{TryFrom, TryInto};
+use std::fmt;
+
+/*
+use tokio::sync::mpsc::{channel, Sender};
+use tokio::sync::oneshot;
+use threshold_crypto::{
+    PublicKeySet, PublicKeyShare, SecretKeySet, SecretKeyShare, SignatureShare,
+};
+use threshold_crypto::serde_impl::SerdeSecret;
+*/
 
 ///////////////////////////////////////////////////////////////////
+
+// Also declared in poly.rs:
 
 /// The scalar field of the pairing groups
 pub type Scalar<E> = <E as PairingEngine>::Fr;
@@ -22,22 +42,245 @@ pub type Polynomial<E> = DensePolynomial<Scalar<E>>;
 ///////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////
 
-/// The target group GT of the pairing
+// EdDSA definitions
+
+pub type CryptoError = ed25519::Error;
+
+#[derive(Hash, PartialEq, Default, Eq, Clone, Deserialize, Serialize)]
+pub struct Digest(pub [u8; 32]);
+
+impl Digest {
+    pub fn to_vec(&self) -> Vec<u8> {
+        self.0.to_vec()
+    }
+
+    pub fn size(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl fmt::Debug for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", base64::encode(&self.0))
+    }
+}
+
+impl fmt::Display for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", base64::encode(&self.0).get(0..16).unwrap())
+    }
+}
+
+impl AsRef<[u8]> for Digest {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl TryFrom<&[u8]> for Digest {
+    type Error = TryFromSliceError;
+    fn try_from(item: &[u8]) -> Result<Self, Self::Error> {
+        Ok(Digest(item.try_into()?))
+    }
+}
+
+pub trait Hash {
+    fn digest(&self) -> Digest;
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Default)]
+pub struct PublicKey(pub [u8; 32]);
+
+impl PublicKey {
+    pub fn to_base64(&self) -> String {
+        base64::encode(&self.0[..])
+    }
+
+    pub fn from_base64(s: &str) -> Result<Self, base64::DecodeError> {
+        let bytes = base64::decode(s)?;
+        let array = bytes[..32]
+            .try_into()
+            .map_err(|_| base64::DecodeError::InvalidLength)?;
+        Ok(Self(array))
+    }
+}
+
+impl fmt::Debug for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.to_base64())
+    }
+}
+
+impl fmt::Display for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.to_base64().get(0..16).unwrap())
+    }
+}
+
+impl Serialize for PublicKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serializer.serialize_str(&self.to_base64())
+    }
+}
+
+impl<'de> Deserialize<'de> for PublicKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let value = Self::from_base64(&s).map_err(|e| de::Error::custom(e.to_string()))?;
+        Ok(value)
+    }
+}
+
+#[derive(Clone)]
+pub struct SecretKey([u8; 64]);
+
+impl SecretKey {
+    pub fn to_base64(&self) -> String {
+        base64::encode(&self.0[..])
+    }
+
+    pub fn from_base64(s: &str) -> Result<Self, base64::DecodeError> {
+        let bytes = base64::decode(s)?;
+        let array = bytes[..64]
+            .try_into()
+            .map_err(|_| base64::DecodeError::InvalidLength)?;
+        Ok(Self(array))
+    }
+}
+
+impl Serialize for SecretKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serializer.serialize_str(&self.to_base64())
+    }
+}
+
+impl<'de> Deserialize<'de> for SecretKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let value = Self::from_base64(&s).map_err(|e| de::Error::custom(e.to_string()))?;
+        Ok(value)
+    }
+}
+
+impl Drop for SecretKey {
+    fn drop(&mut self) {
+        self.0.iter_mut().for_each(|x| *x = 0);
+    }
+}
+
+pub fn generate_production_keypair() -> (PublicKey, SecretKey) {
+    generate_keypair(&mut OsRng)
+}
+
+pub fn generate_keypair<R>(csprng: &mut R) -> (PublicKey, SecretKey)
+where
+    R: CryptoRng + RngCore,
+{
+    let keypair = dalek::Keypair::generate(csprng);
+    let public = PublicKey(keypair.public.to_bytes());
+    let secret = SecretKey(keypair.to_bytes());
+    (public, secret)
+}
+
+// Struct representing an EdDSA signature.
+#[derive(Clone, Default, Debug)]
+pub struct Signature {
+    part1: [u8; 32],
+    part2: [u8; 32],
+}
+
+impl Signature {
+    pub fn new(digest: &Digest, secret: &SecretKey) -> Self {
+        let keypair = dalek::Keypair::from_bytes(&secret.0).expect("Unable to load secret key");
+        let sig = keypair.sign(&digest.0).to_bytes();
+        let part1 = sig[..32].try_into().expect("Unexpected signature length");
+        let part2 = sig[32..64].try_into().expect("Unexpected signature length");
+        Signature { part1, part2 }
+    }
+
+    fn flatten(&self) -> [u8; 64] {
+        [self.part1, self.part2]
+            .concat()
+            .try_into()
+            .expect("Unexpected signature length")
+    }
+
+    pub fn verify(&self, digest: &Digest, public_key: &PublicKey) -> Result<(), CryptoError> {
+        let signature = ed25519::signature::Signature::from_bytes(&self.flatten())?;
+        let key = dalek::PublicKey::from_bytes(&public_key.0)?;
+        key.verify_strict(&digest.0, &signature)
+    }
+
+    pub fn verify_batch<'a, I>(digest: &Digest, votes: I) -> Result<(), CryptoError>
+    where
+        I: IntoIterator<Item = &'a (PublicKey, Signature)>,
+    {
+        let mut messages: Vec<&[u8]> = Vec::new();
+        let mut signatures: Vec<dalek::Signature> = Vec::new();
+        let mut keys: Vec<dalek::PublicKey> = Vec::new();
+        for (key, sig) in votes.into_iter() {
+            messages.push(&digest.0[..]);
+            signatures.push(ed25519::signature::Signature::from_bytes(&sig.flatten())?);
+            keys.push(dalek::PublicKey::from_bytes(&key.0)?);
+        }
+        dalek::verify_batch(&messages[..], &signatures[..], &keys[..])
+    }
+
+
+    // Added to enable serialization and deserialization.
+    pub fn to_base64(&self) -> String {
+        base64::encode(&self.flatten())
+    }
+
+    
+}
+
+impl Serialize for Signature {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serializer.serialize_str(&self.to_base64())
+    }
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// The target group GT of the pairing
 pub type GT<E> = <E as PairingEngine>::Fqk;
 
-/// The secret that we will be encoding
-/// Also the beacon
+// The secret that we will be encoding
+// Also the beacon
 pub type Secret<E> = GT<E>;
 
-/// The Share type
-pub type Share<E> = Encryptions<E>;
+// The Share type
+// pub type Share<E> = Encryptions<E>;
 pub type Commitment<E> = <E as PairingEngine>::G1Affine;
 
 pub type CommitmentP<E> = <E as PairingEngine>::G1Projective;
-pub type SecretKey<E> = Scalar<E>;
-pub type PublicKey<E> = <E as PairingEngine>::G2Projective;
+//pub type SecretKey<E> = Scalar<E>;
+//pub type PublicKey<E> = <E as PairingEngine>::G2Projective;
 
-/// The Encryption group is the same as the public key group
-/// Which is G1 for type 3 pairings
-pub type Encryptions<E> = PublicKey<E>;
+// The Encryption group is the same as the public key group
+// Which is G1 for type 3 pairings
+//pub type Encryptions<E> = PublicKey<E>;
