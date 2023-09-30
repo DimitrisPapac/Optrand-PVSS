b491f4d7e4add0c46f44ada87996a532e0630d9d
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-09-30 23:22:19 +0200

    Update participant.rs

--- src/modified_scrape/participant.rs
+++ src/modified_scrape/participant.rs
@@ -2,6 +2,7 @@ use crate::PublicKey;
 use ark_ec::PairingEngine;
 use std::marker::PhantomData;
 use crate::signature::scheme::BatchVerifiableSignatureScheme;
+use ark_serialize::{CanonicalSerialize, CanonicalDeserialize, SerializationError, Read, Write};
 
 // Struct ParticipantState models the states that each participant in the PVSS
 // scheme goes through.
@@ -14,7 +15,7 @@ pub enum ParticipantState {
 }
 
 // Struct Participant models each individual party participating in the PVSS scheme.
-#[derive(Clone)]
+#[derive(Clone, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Participant<
     E: PairingEngine,
     SSIG: BatchVerifiableSignatureScheme<PublicKey = E::G1Affine, Secret = E::Fr>,   // G1 is the encryption group
@@ -23,5 +24,5 @@ pub struct Participant<
     pub id: usize,                         // participant id
     pub public_key_sig: SSIG::PublicKey,   // public (encryption) key (in G1)
     pub public_key_ed: PublicKey,          // EdDSA public (verification) key
-    pub state: ParticipantState,           // participant current state
+    // pub state: ParticipantState,           // participant current state
 }
