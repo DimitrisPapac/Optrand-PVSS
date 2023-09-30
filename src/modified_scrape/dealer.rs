7740ccf09c3a3e60842b172b8b384578ca578614
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-09-30 12:16:55 +0200

    Update dealer.rs
    Removed redundant fields.

--- src/modified_scrape/dealer.rs
+++ src/modified_scrape/dealer.rs
@@ -12,9 +12,4 @@ pub struct Dealer<
     pub private_key_sig: SSIG::Secret,       // Dealer's secret (decryption) key
     pub private_key_ed: SecretKey,           // EdDSA secret (signing) key
     pub participant: Participant<E, SSIG>,   // Dealers have participant characteristics (structural composition)
-
-    // POSSIBLY REDUNDANT
-    pub accumulated_secret: E::G2Affine,     // Dealer's accumulated secret (in G_2)
-    // POSSIBLY REDUNDANT
-    pub decryptions: Vec<(usize, E::G1Affine)>,   // Dealer's list of accumulated decryptions, along with the respective participant ids
 }
