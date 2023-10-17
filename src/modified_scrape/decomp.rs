c4982991d3244199ed56c92704af32c206fb002b
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-10-17 15:13:40 +0200

    Update decomp.rs

--- src/modified_scrape/decomp.rs
+++ src/modified_scrape/decomp.rs
@@ -49,7 +49,7 @@ impl<E: PairingEngine> Decomp<E> {
 	let dlk_srs = DLKSRS::<ProofGroup::<E>> { g_public_key: generator };
 	let dlk = DLKProof { srs: dlk_srs };   // initialize proof system for DLK NIZKs.
 
-	let proof = dlk.prove(rng, &secret).unwrap();
+	let proof = dlk.prove(rng, secret).unwrap();
 
 	Ok(DecompProof { proof, gs })
     }
