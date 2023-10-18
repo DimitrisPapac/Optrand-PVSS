fdc2288d7fbb425e74435ca83ffc929ffb5f0006
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-10-18 15:58:25 +0200

    Update aggregator.rs

--- src/modified_scrape/aggregator.rs
+++ src/modified_scrape/aggregator.rs
@@ -78,34 +78,36 @@ where
         core: &PVSSCore<E>,
     ) -> Result<(), PVSSError<E>> {
 
-	// Check that the sizes of commitments and encryptions are correct.
-	if core.encs.len() != self.config.num_participants ||
+	    // Check that the sizes of commitments and encryptions are correct.
+	    if core.encs.len() != self.config.num_participants ||
            core.comms.len() != self.config.num_participants {
 	        return Err(PVSSError::MismatchedCommitsEncryptionsParticipantsError(core.encs.len(),
 			    core.comms.len(), self.config.num_participants));
-	}
+	    }
 
-	// Coding check for the commitments to ensure that they represent a
-	// commitment to a degree t polynomial.
-	if ensure_degree::<E, _>(rng, &core.comms, self.config.degree as u64).is_err() {
+	    // Coding check for the commitments to ensure that they represent a
+	    // commitment to a degree t polynomial.
+	    if ensure_degree::<E, _>(rng, &core.comms, self.config.degree as u64).is_err() {
             return Err(PVSSError::DualCodeError);
         }
 
-	// The pairing condition for correctness of encryption is: e(pk_i, v_i) = e(enc_i, g_2).
-	// NOTE: However, we do not have access to the sender's identity at this point (and by
-	// extension, its public key). Hence, this check is carried out in share_verify.
+	    // The pairing condition for correctness of encryption is: e(pk_i, v_i) = e(enc_i, g_2).
+	    // NOTE: However, we do not have access to the sender's identity at this point (and by
+	    // extension, its public key). Hence, this check is carried out in share_verify.
 
         // Check decomposition proof.
-	let point = lagrange_interpolation_simple::<E>(&core.comms, self.config.degree as u64).unwrap();   // E::G2Projective
+	    let point = lagrange_interpolation_simple::<E>(
+            &core.comms, self.config.degree as u64).unwrap();   // E::G2Projective
 
-	if point != decomp_proof.gs {
+	    if point != decomp_proof.gs {
 	        return Err(PVSSError::GSCheckError);
-	}
+	    }
 
-	// Verify decomposition proof against our config.
-        if decomp_proof.verify(&self.config).is_err() {
-	    return Err(PVSSError::DecompProofVerificationError);
-	}
+        // Verification is now performed from within SignedProof::verify()
+	    // Verify decomposition proof against our config.
+        // if decomp_proof.verify(&self.config).is_err() {
+	    //     return Err(PVSSError::DecompProofVerificationError);
+	    // }
 
         Ok(())
     }
@@ -140,12 +142,17 @@ where
         // Verify the "core" PVSS share against the provided decomposition proof.
         self.core_verify(rng, &share.signed_proof.decomp_proof, &share.pvss_core)?;
 
-        // Verify signature on decomposition proof against participant i's public key:
-        let digest = share.signed_proof.decomp_proof.digest();
-
-        if share.signed_proof.signature_on_decomp.verify(&digest, &participant.public_key_ed).is_err() {
-            return Err(PVSSError::EdDSAInvalidSignatureError);
+        // Verify proof and signature on decomposition proof against participant i's public key:
+        if share.signed_proof.verify(&self.config, &participant.public_key_ed).is_err() {
+            return Err(PVSSError::InvalidSignedProofError);
         }
+
+        // The following check is now delegated to SignedProof::verify()
+        //let digest = share.signed_proof.decomp_proof.digest();
+
+        //if share.signed_proof.signature_on_decomp.verify(&digest, &participant.public_key_ed).is_err() {
+        //    return Err(PVSSError::EdDSAInvalidSignatureError);
+        //}
 
         Ok(())
     }
@@ -168,67 +175,64 @@ where
                             self.config.num_participants));
         }
 
-    // if agg_share.contributions.len() < self.config.degree {}
+        // if agg_share.contributions.len() < self.config.degree {}
 
-	// Coding check for the commitments to ensure that they represent a
-	// commitment to a degree t polynomial.
-	if ensure_degree::<E, _>(rng, &agg_share.pvss_core.comms, self.config.degree as u64).is_err() {
+	    // Coding check for the commitments to ensure that they represent a
+	    // commitment to a degree t polynomial.
+	    if ensure_degree::<E, _>(rng, &agg_share.pvss_core.comms, self.config.degree as u64).is_err() {
             return Err(PVSSError::DualCodeError);
         }
 	
-	// Pairing check: e(pk_i, com_i) = e(enc_i, g2).
+	    // Pairing check: e(pk_i, com_i) = e(enc_i, g2).
 
-	let correct_encryptions = (0..self.config.num_participants)
-	        .all(|i| { let pairs: [(<E as PairingEngine>::G1Prepared, <E as PairingEngine>::G2Prepared); 2] = [
+	    let correct_encryptions = (0..self.config.num_participants)
+	        .all(|i| { let pairs = [
             	    (self.participants.get(&i).unwrap().public_key_sig.into(), agg_share.pvss_core.comms[i].into()),
             	    (agg_share.pvss_core.encs[i].neg().into(), self.config.srs.g2.into()),
         	];
 
 		    E::product_of_pairings(pairs.iter()).is_one()
+	        }
+	    );
+
+	    if !correct_encryptions {
+	        return Err(PVSSError::EncryptionCorrectnessError);
 	    }
-	);
 
-	if !correct_encryptions {
-	    return Err(PVSSError::EncryptionCorrectnessError);
-	}
+	    // Decomposition proof check:
 
-	// Decomposition proof check:
+	    let point = lagrange_interpolation_simple::<E>(&agg_share.pvss_core.comms, self.config.degree as u64).unwrap();   // E::G2Projective
 
-	let point = lagrange_interpolation_simple::<E>(&agg_share.pvss_core.comms, self.config.degree as u64).unwrap();   // E::G2Projective
-
-	//let mut gs_total = E::G2Affine::zero();
+	    //let mut gs_total = E::G2Affine::zero();
         let mut gs_total = E::G2Projective::zero();
 
-	// Contributions are essentially signed decomposition proofs.
-	for (_participant_id, contribution) in agg_share.contributions.iter() {
-	    if contribution.decomp_proof.verify(&self.config).is_err() {
-		return Err(PVSSError::DecompositionInTranscriptError);
-	    }
+	    // Contributions are essentially signed decomposition proofs.
+	    for (_participant_id, contribution) in agg_share.contributions.iter() {
+            // let party = self.participants.get(participant_id).unwrap();
+            // if contribution.verify(&self.config, &party.public_key_ed).is_err() {
+            //     return Err(PVSSError::InvalidSignedProofError);
+            // }
+
+	        if contribution.decomp_proof.verify(&self.config).is_err() {
+		        return Err(PVSSError::DecompositionInTranscriptError);
+	        }
 
             //gs_total += contribution.decomp_proof.gs;
             gs_total.add_assign_mixed(&contribution.decomp_proof.gs);
-	}
+	    }
 
 	    if gs_total.into_affine() != point {   // if gs_total != point.into_affine()
 	        return Err(PVSSError::AggregationReconstructionMismatchError);
 	    }
 
 	    for (participant_id, contribution) in agg_share.contributions.iter() {
-            let party = self.participants.get(participant_id).unwrap();
+           let party = self.participants.get(participant_id).unwrap();
 
-            // Verify individual signed proof
-            if contribution.clone().verify(&self.config, &party.public_key_ed).is_err() {
-                return Err(PVSSError::EdDSAInvalidSignatureError);
-            }
+           // Verify individual signed proof
+           if contribution.clone().verify(&self.config, &party.public_key_ed).is_err() {
+               return Err(PVSSError::EdDSAInvalidSignatureError);
+           }
 	    }
-
-	    //let digest = Digest(arr);
-	    //let votes: Zip<Iter<'_, PublicKey>, Iter<'_, Signature>> = pks.iter().zip(sigs.iter());
-	
-	    // ERROR OCCURS IN THE FOLLOWING CALL TO THE DALEK LIBRARY!!!
-	    //if Signature::verify_batch(&digest, votes).is_err() {
-	    //	return Err(PVSSError::EdDSAInvalidSignatureBatchError);
-	    //}
 
         Ok(())
     }
@@ -242,7 +246,7 @@ where
     ) -> Result<(), PVSSError<E>> {
 
         // Verify the PVSS share.
-        self.share_verify(rng, share)?;
+        self.share_verify(rng, share).unwrap();
 
         // Aggregate the PVSS share into the aggregator's internal aggregated transcript.
         self.aggregated_tx = self.aggregated_tx.aggregate_pvss_share(share)?;
@@ -260,7 +264,7 @@ where
     ) -> Result<(), PVSSError<E>> {
 
 	// Verify aggregation
-	self.aggregation_verify(rng, agg_share)?;
+	self.aggregation_verify(rng, agg_share).unwrap();
 
 	// Aggregate the received aggregated PVSS share into the aggregator's internal aggregated transcript.
 	self.aggregated_tx = self.aggregated_tx.aggregate(agg_share).unwrap();
