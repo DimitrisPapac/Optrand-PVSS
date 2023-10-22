d9de8d7271697566b0d6066fcb12582ce76219ec
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-10-23 00:51:09 +0200

    Update aggregator.rs
    Added weights

--- src/modified_scrape/aggregator.rs
+++ src/modified_scrape/aggregator.rs
@@ -14,7 +14,7 @@ use crate::{
     signature::scheme::BatchVerifiableSignatureScheme,
 };
 
-use ark_ec::{PairingEngine, ProjectiveCurve};
+use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve};
 use ark_ff::{One, Zero};
 use ark_std::collections::BTreeMap;
 
@@ -139,7 +139,9 @@ where
         &mut self,
         rng: &mut R,
         agg_share: &PVSSAggregatedShare<E>,
-    ) -> Result<(), PVSSError<E>> {
+    ) -> Result<(), PVSSError<E>> 
+    where
+        Scalar<E>: From<u64> {
 
         // Check that the sizes of commitments and encryptions are correct.
         if agg_share.pvss_core.encs.len() != self.config.num_participants ||
@@ -152,62 +154,65 @@ where
 
         // if agg_share.contributions.len() < self.config.degree {}
 
-	    // Coding check for the commitments to ensure that they represent a
-	    // commitment to a degree t polynomial.
-	    if ensure_degree::<E, _>(rng, &agg_share.pvss_core.comms, self.config.degree as u64).is_err() {
+	// Coding check for the commitments to ensure that they represent a
+	// commitment to a degree t polynomial.
+	if ensure_degree::<E, _>(rng, &agg_share.pvss_core.comms, self.config.degree as u64).is_err() {
             return Err(PVSSError::DualCodeError);
         }
 	
-	    // Pairing check: e(pk_i, com_i) = e(enc_i, g2).
+	// Pairing check: e(pk_i, com_i) = e(enc_i, g2).
 
-	    let correct_encryptions = (0..self.config.num_participants)
-	        .all(|i| { let pairs = [
-            	    (self.participants.get(&i).unwrap().public_key_sig.into(), agg_share.pvss_core.comms[i].into()),
-            	    (agg_share.pvss_core.encs[i].neg().into(), self.config.srs.g2.into()),
-        	];
+	let correct_encryptions = (0..self.config.num_participants)
+	    .all(|i| { let pairs = [
+            	(self.participants.get(&i).unwrap().public_key_sig.into(), agg_share.pvss_core.comms[i].into()),
+            	(agg_share.pvss_core.encs[i].neg().into(), self.config.srs.g2.into()),
+            ];
 
-		    E::product_of_pairings(pairs.iter()).is_one()
-	        }
-	    );
+	    E::product_of_pairings(pairs.iter()).is_one()
+	    }
+	);
 
-	    if !correct_encryptions {
-	        return Err(PVSSError::EncryptionCorrectnessError);
+	if !correct_encryptions {
+	    return Err(PVSSError::EncryptionCorrectnessError);
+	}
+
+	// Decomposition proof check:
+
+	let point = lagrange_interpolation_simple::<E>(&agg_share.pvss_core.comms,
+        self.config.degree as u64).unwrap();
+
+	// let mut gs_total = ComGroup::<E>::zero();
+        let mut gs_total = ComGroupP::<E>::zero();
+
+	// Contributions are essentially signed decomposition proofs along with their weight.
+	for (_participant_id, (contribution, weight)) in agg_share.contributions.iter() {
+        // let party = self.participants.get(participant_id).unwrap();
+        // if contribution.verify(&self.config, &party.public_key_ed).is_err() {
+        //     return Err(PVSSError::InvalidSignedProofError);
+        // }
+
+	    if contribution.decomp_proof.verify(&self.config).is_err() {
+		return Err(PVSSError::DecompositionInTranscriptError);
 	    }
 
-	    // Decomposition proof check:
+            gs_total += contribution.decomp_proof.gs.mul(Scalar::<E>::from(*weight));
+	}
 
-	    let point = lagrange_interpolation_simple::<E>(&agg_share.pvss_core.comms, self.config.degree as u64).unwrap();   // E::G2Projective
+    // The point reconstructed from the aggregated share's commitment vector must be a
+    // commitment to the evaluation of polynomial sum_{i} w_i * p_i(x) on point x = 0
+    // i.e., a commitment to this polynomial's free term.
+	if gs_total.into_affine() != point {   // if gs_total != point.into_affine()
+	    return Err(PVSSError::AggregationReconstructionMismatchError);
+	}
 
-	    // let mut gs_total = ComGroup::<E>::zero();
-            let mut gs_total = ComGroupP::<E>::zero();
+	for (participant_id, (contribution, _weight)) in agg_share.contributions.iter() {
+            let party = self.participants.get(participant_id).unwrap();
 
-	    // Contributions are essentially signed decomposition proofs.
-	    for (_participant_id, contribution) in agg_share.contributions.iter() {
-            // let party = self.participants.get(participant_id).unwrap();
-            // if contribution.verify(&self.config, &party.public_key_ed).is_err() {
-            //     return Err(PVSSError::InvalidSignedProofError);
-            // }
-
-	        if contribution.decomp_proof.verify(&self.config).is_err() {
-		        return Err(PVSSError::DecompositionInTranscriptError);
-	        }
-
-            //gs_total += contribution.decomp_proof.gs;
-            gs_total.add_assign_mixed(&contribution.decomp_proof.gs);
-	    }
-
-	    if gs_total.into_affine() != point {   // if gs_total != point.into_affine()
-	        return Err(PVSSError::AggregationReconstructionMismatchError);
-	    }
-
-	    for (participant_id, contribution) in agg_share.contributions.iter() {
-           let party = self.participants.get(participant_id).unwrap();
-
-           // Verify individual signed proof
-           if contribution.clone().verify(&self.config, &party.public_key_ed).is_err() {
+            // Verify individual signed proof
+            if contribution.clone().verify(&self.config, &party.public_key_ed).is_err() {
                return Err(PVSSError::EdDSAInvalidSignatureError);
-           }
-	    }
+            }
+	}
 
         Ok(())
     }
