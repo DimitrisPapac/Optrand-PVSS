81e7205e768d284c8679b3a331bbe1727af901d6
DimitrisPapac <jim.papachristoudis@gmail.com>
2023-11-24 00:15:55 +0100

    Update config.rs

--- src/modified_scrape/config.rs
+++ src/modified_scrape/config.rs
@@ -1,11 +1,12 @@
 use super::srs::SRS;
 use ark_ec::PairingEngine;
+use ark_serialize::{CanonicalSerialize, CanonicalDeserialize, Read, SerializationError, Write};
 
 /* Struct config models the system-wide public parameters that each party
    in the network needs to know in order to generate/verify a PVSS sharing.
 */
 
-#[derive(Clone)]
+#[derive(Clone, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Config<E: PairingEngine> {
     pub srs: SRS<E>,               // the associated SRS
     pub degree: usize,             // polynomial degree (t)
